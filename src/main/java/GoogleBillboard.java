64930c797fd0f2a841e55f5bd5f5acad467461c8
kevlao1 <kevinlao2k20@gmail.com>
2025-01-08 12:03:36 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,12 +1,13 @@
-public class GoogleBillboard {
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-public static void main(final String[] args){
-System.out.println("7.427466391E9");
-}
-public static boolean isPrime(double d){
-if(d <= 1){return false;}
-for(int i = 2; i <= Math.sqrt(d); i++){
-if(d%i == 0)return false;
-}
-return true;   
-}
+public class GoogleBillboard{
+  public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+  public static void main(final String[] args){
+    System.out.println("7.427466391E9");
+  }
+  public static boolean isPrime(double d){
+    if(d<=1){return false;}
+    for(int i=2; i<=Math.sqrt(d); i++){
+      if(d%i==0) return false;
+    }
+    return true;
+  }
+
