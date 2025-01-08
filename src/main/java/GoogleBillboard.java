7d959b6279d6c40a8cf52cbc6473a85da3cd50f4
kevlao1 <kevinlao2k20@gmail.com>
2025-01-08 11:46:18 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -9,10 +9,9 @@ public class GoogleBillboard {
 
 	//Finish this function
 public static boolean isPrime(double d){
-   if(d <= 1){return false;}
-  for(int i = 2; i <= Math.sqrt(d); i++){
-    if(d%i == 0)return false;
+if(d <= 1){return false;}
+for(int i = 2; i <= Math.sqrt(d); i++){
+if(d%i == 0)return false;
   }
   return true;   
 }
-
